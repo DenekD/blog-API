e0ec3d1c46f712c401d56fe8622f3f35132a52f0
Barnaba <Denek20@gmail.com>
2020-05-28 11:58:43 +0200

    Updates

---
+++ precache-manifest.433586cff08c26a4489f4609a48c0905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9201dfd5c84d9f318f25f3abe5ed8d69",
+    "url": "//denekd.github.io/blog-API/index.html"
+  },
+  {
+    "revision": "b72d114c36c1943df9a5",
+    "url": "//denekd.github.io/blog-API/static/css/main.e27f1f02.chunk.css"
+  },
+  {
+    "revision": "f5967f0feb23af14e48d",
+    "url": "//denekd.github.io/blog-API/static/js/2.fa88592c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//denekd.github.io/blog-API/static/js/2.fa88592c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72d114c36c1943df9a5",
+    "url": "//denekd.github.io/blog-API/static/js/main.94a26783.chunk.js"
+  },
+  {
+    "revision": "5aef5531cbcd494fba79",
+    "url": "//denekd.github.io/blog-API/static/js/runtime-main.0a0841bb.js"
+  }
+]);
